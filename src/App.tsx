26a410706b1943333ecb64a3aae8bdbf8e9176a0
btkcodedev <btk.codedev@gmail.com>
2025-01-07 21:57:45 +0530

    feat: make vite_tailwind_firebase bolierplate

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import WelcomeScreen from './components/index/WelcomeScreen';
+import Onboarding from './components/index/Onboarding';
+import Dashboard from './components/index/Dashboard';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<WelcomeScreen />} />
+        <Route path="/onboarding" element={<Onboarding />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;

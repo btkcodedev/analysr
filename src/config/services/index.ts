abd3951968c1cc4db370cf9262ad2c82d951b8e5
btkcodedev <btk.codedev@gmail.com>
2025-01-10 15:15:25 +0530

    update proxy to supabase

--- src/config/services/index.ts
+++ src/config/services/index.ts
@@ -1,6 +1,6 @@
-export const AIRBYTE_API_PROXY_URL = "/api/airbyte";
+export const AIRBYTE_API_PROXY_URL = "https://lejxudxaxuqfkhtgddoe.supabase.co/functions/v1/apiProxy/airbyte";
 export const AIRBYTE_API_BASE_URL = "https://api.airbyte.com/v1";
-export const GROQ_API_PROXY_URL = "/api/groq";
+export const GROQ_API_PROXY_URL = "https://lejxudxaxuqfkhtgddoe.supabase.co/functions/v1/apiProxy/groq";
 export const GROQ_API_BASE_URL = "https://api.groq.com/v1";
 
 export const getAirbyteApiUrl = () => {

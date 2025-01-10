aad2d38ef76d9b011dd4ce81ae89dbc198901ce6
btkcodedev <btk.codedev@gmail.com>
2025-01-10 14:36:03 +0530

    fix: revert back airbyte url to url proxy server

--- src/config/services/index.ts
+++ src/config/services/index.ts
@@ -4,15 +4,19 @@ export const GROQ_API_PROXY_URL = "/api/groq";
 export const GROQ_API_BASE_URL = "https://api.groq.com/v1";
 
 export const getAirbyteApiUrl = () => {
-    if (import.meta.env.DEV) {
-      return AIRBYTE_API_PROXY_URL;
-    }
-    return AIRBYTE_API_BASE_URL;
+    // if (import.meta.env.DEV) {
+    //   return AIRBYTE_API_PROXY_URL;
+    // }
+    // return AIRBYTE_API_BASE_URL;
+
+    return AIRBYTE_API_PROXY_URL;
   };
   
   export const getGroqApiUrl = () => {
-    if (import.meta.env.DEV) {
-      return GROQ_API_PROXY_URL;
-    }
-    return GROQ_API_BASE_URL;
+    // if (import.meta.env.DEV) {
+    //   return GROQ_API_PROXY_URL;
+    // }
+    // return GROQ_API_BASE_URL;
+
+    return GROQ_API_PROXY_URL;
   };

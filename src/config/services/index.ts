f7d907c515df36403e5c2335d8163b17f0aae79d
btkcodedev <btk.codedev@gmail.com>
2025-01-11 15:09:40 +0530

    update workflows to use VITE_

--- src/config/services/index.ts
+++ src/config/services/index.ts
@@ -7,12 +7,12 @@ export const getAirbyteApiUrl = () => {
     if (import.meta.env.DEV) {
       return AIRBYTE_LOCAL_PROXY_URL;
     }
-    return import.meta.env.AIRBYTE_SUPABASE_PROXY_URL;
+    return import.meta.env.VITE_AIRBYTE_SUPABASE_PROXY_URL;
   };
   
   export const getGroqApiUrl = () => {
     if (import.meta.env.DEV) {
       return GROQ_LOCAL_PROXY_URL;
     }
-    return import.meta.env.GROQ_SUPABASE_PROXY_URL;
+    return import.meta.env.VITE_GROQ_SUPABASE_PROXY_URL;
   };
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,7 +1,9 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_MOTHERDUCK_TOKEN: string
+  readonly VITE_MOTHERDUCK_TOKEN: string;
+  readonly VITE_AIRBYTE_SUPABASE_PROXY_URL: string;
+  readonly VITE_GROQ_SUPABASE_PROXY_URL: string;
 }
 
 interface ImportMeta {
